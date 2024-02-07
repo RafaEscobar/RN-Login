888b7f02f29a2115b0457f3d555b7b5bbc472c63
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-02-07 00:21:14 -0600

    Navegación base

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import { Login } from '../screens';
+import { Dashboard } from '../screens/Dashboard';
+
+const Stack = createStackNavigator();
+
+export const StackNavigation = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="dashboard" component={Dashboard} />
+        </Stack.Navigator>
+    )
+}
